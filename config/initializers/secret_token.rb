f85ef81c33fccd81963db5e599414a10ae0f3302
QuixoticLass <gracefulgumshoe@gmail.com>
2013-08-07 22:32:05 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tardis::Application.config.secret_key_base = 'c93a08fec1ebea3abea63220a6e28fdf9e7f63df3f794938a6ca8a6972ca4948e0fa03be43578941c9041a41ccf9d55045340abc8dcac3a1b9fe4474c067b93b'
